54142162f498befc6fb87846eb3887f3c5a49fb7
danielme-com <daniel_com@yahoo.com>
2015-08-22 17:20:13 +0200

    AppCompatActivity

--- src/com/danielme/blog/demo/listviewcheckbox/MainActivity.java
+++ src/com/danielme/blog/demo/listviewcheckbox/MainActivity.java
@@ -3,10 +3,11 @@ package com.danielme.blog.demo.listviewcheckbox;
 import java.util.ArrayList;
 import java.util.List;
 
-import android.app.ListActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ListView;
 import android.widget.Toast;
 import android.widget.AdapterView.OnItemClickListener;
 
@@ -15,37 +16,41 @@ import android.widget.AdapterView.OnItemClickListener;
  * @author danielme.com
  * 
  */
-public class MainActivity extends ListActivity
-{
+public class MainActivity extends AppCompatActivity {
 	List<Row> rows;
-	
+
+	private ListView listView;
+
 	@Override
-	protected void onCreate(Bundle savedInstanceState)
-	{
+	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.listview);
+		listView = (ListView) findViewById(android.R.id.list);
+		
+		
 		rows = new ArrayList<Row>(30);
 		Row row = null;
-		for (int i = 1; i < 31; i++)
-		{
+		for (int i = 1; i < 31; i++) {
 			row = new Row();
 			row.setTitle("Title " + i);
 			row.setSubtitle("Subtitle " + i);
 			rows.add(row);
 		}
-		
+
 		rows.get(3).setChecked(true);
 		rows.get(6).setChecked(true);
 		rows.get(9).setChecked(true);
-		 
-		setListAdapter(new CustomArrayAdapter(this, rows));
-		
-		getListView().setOnItemClickListener(new OnItemClickListener() {
+
+		listView.setAdapter(new CustomArrayAdapter(this, rows));
+
+		listView.setOnItemClickListener(new OnItemClickListener() {
 
 			@Override
-			public void onItemClick(AdapterView<?> parent, View view, int position, long id)
-			{
-				Toast.makeText(MainActivity.this, rows.get(position).getTitle(), Toast.LENGTH_SHORT).show();				
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+				Toast.makeText(MainActivity.this,
+						rows.get(position).getTitle(), Toast.LENGTH_SHORT)
+						.show();
 			}
 		});
 	}
